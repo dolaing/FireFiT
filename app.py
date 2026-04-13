73f4561f369691dac9c07f9476c27a6bd866d285
Dario laing <dolaing@seu.edu>
2026-04-13 13:17:26 -0400

    Set-up Logins

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print " Welcome to the FireFit App where all coding"
+print("Welcome to the FireFit Family!")
+*******
+def signup():
+    print("🔥 Welcome to FireFit Sign Up 🔥")
+    
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    dob = input("Enter your date of birth (MM/DD/YYYY): ")
+    height = input("Enter your height (e.g. 5'10 or 178cm): ")
+    weight = input("Enter your weight: ")
+    password = input("Create a password: ")
