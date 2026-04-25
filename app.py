cb38ab9497a7fb502932ae607f82f23cba20dab5
Dario laing <dolaing@seu.edu>
2026-04-25 08:31:46 -0400

    The Basic Set Up for users have been created

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 print("Welcome to the FireFit Family!")
-*******
-def signup():
-    print("🔥 Welcome to FireFit Sign Up 🔥")
-    
-    first_name = input("Enter your first name: ")
-    last_name = input("Enter your last name: ")
-    dob = input("Enter your date of birth (MM/DD/YYYY): ")
-    height = input("Enter your height (e.g. 5'10 or 178cm): ")
-    weight = input("Enter your weight: ")
-    password = input("Create a password: ")
+
+#Ask the user to input information upon downloading the app
+class UserProfile:
+  def __init__(self, name, age, weight, height, goal, activity_level):
+        self.name = name
+        self.age = age
+        self.weight = weight  # in pounds or kg
+        self.height = height  # inches or cm
+        self.goal = goal  # "fat_loss", "muscle_gain", "endurance"
+        self.activity_level = activity_level  # "low", "moderate", "high"
+
+#Display the profile for the user to see
+def display_profile(self):
+        return {
+            "Name": self.name,
+            "Age": self.age,
+            "Weight": self.weight,
+            "Height": self.height,
+            "Goal": self.goal,
+            "Activity Level": self.activity_level
+        }
+
+# Ask the user what Activity Level would they like to start at
+def get_activity_multiply (activity_level):
+      levels= {
+            "low": 1.2,
+             "moderate": 1.55,
+             "high": 1.9
+      }
+      return levels.get(activity_level, 1.2)
+      
+   
