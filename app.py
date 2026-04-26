966ce31ab30233c3f580208a9c53b0f53185008e
Dario laing <dolaing@seu.edu>
2026-04-25 22:47:10 -0400

    The Basic Set Up for users have been created

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def get_goal_recommendation(goal):
 
 def estimate_calories(weight, activity_level):
     base_calories = weight * 14  # simple baseline
-    multiplier = get_activity_multiply(activity_level)
+    multiplier = get_activity_multiply (activity_level)
     return int(base_calories * multiplier)
 
 #Provide a user summary
@@ -59,4 +59,52 @@ def generate_user_summary(user):
         "Recommendation": recommendation
     }
 
-   
+   ####### User Set is done , now its time for Logging Data#######
+
+   #class Exercise:
+    def __init__(self, name, set, reps, weight):
+        self.name = name
+        self.set = set
+        self.reps = reps
+        self.weight = weight
+
+    def display(self):
+        return {
+            "exercise": self.name,
+            "sets": self.sets,
+            "reps": self.reps,
+            "weight": self.weight
+        }
+#Once the information is added to the app its time to tack it
+class WorkoutLogger:
+    def __init__(self):
+        self.workouts = []
+
+    def log_workout(self, workout):
+        self.workouts.append(workout)
+
+    def get_all_workouts(self):
+        return [w.display_workout() for w in self.workouts]
+
+    def get_user_workouts(self, user_id):
+        return [w.display_workout() for w in self.workouts if w.user_id == user_id]
+    
+#Performance tracking should be added to each members account
+def get_exercise_progress(workouts, exercise_name):
+    history = []
+
+    for workout in workouts:
+        for ex in workout.exercises:
+            if ex.name == exercise_name:
+                history.append({
+                    "date": workout.date,
+                    "weight": ex.weight,
+                    "reps": ex.reps
+                })
+
+    return history
+
+#This is important for the users because it ensures that it tracks the progress of each user, so that they can review it for later.
+
+#Create the basic dashboard for the app
+#Import Matplotlib to help with prototype charts and graphs of progression
